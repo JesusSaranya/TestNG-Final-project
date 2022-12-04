e0f9be60548ae8b1e98edb078a987d4aebb6d04d
saranyathiagrajan <subramaniyansaranya26@gmail.com>
2022-12-04 18:38:07 -0500

    This is my testng

---
+++ src/main/java/page/BasePage.java
@@ -0,0 +1,25 @@
+package page;
+
+import java.util.Random;
+
+public class BasePage {
+
+	
+	
+	public int generateRandomNum(int bountryNum)
+	{
+		Random rd = new Random();
+	   int generateNum =  rd.nextInt();
+	   return generateNum;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
